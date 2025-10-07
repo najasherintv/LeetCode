2f2011527781b1e72f0f41f9a6aa97474ae60401
najasherintv <najasherintv@gmail.com>
2025-10-07 12:45:54 +0530

    Time: 4 ms (17.39%), Space: 12.3 MB (85.99%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        return len(set(sentence))== 26
+        
