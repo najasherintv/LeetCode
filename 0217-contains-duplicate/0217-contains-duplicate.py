50c61da89d53da73479a304b240891271bbba6d5
najasherintv <najasherintv@gmail.com>
2025-10-14 10:13:41 +0530

    Time: 20 ms (45.82%), Space: 26.1 MB (18.14%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hash_set = set()
+        for num in nums:
+            if num in hash_set:
+                return True
+            hash_set.add(num)
+        return False    
+
